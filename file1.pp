c48a282216ab1e62fb1aefb4da0e540d55911b6b
dfr0 <frodorik21@gmail.com>
2015-07-28 11:23:06 +0200

    tercera prueba

--- file1.pp
+++ file1.pp
@@ -1,6 +1,24 @@
-file {'testfile' :
-  path	=> '/tmp/testfile',
-  ensure => present,
+file {'testfile1' :
+  path	=> '/tmp/testfile1',
+  ensure => file,
   mode => 0640,
-  content => "i'm a test file",
+  content => "hi1",
 }
+
+file {'testfile2' :
+  path  => '/tmp/testfile2',
+  ensure => directory,
+  mode => 0644,
+}
+
+file {'testfile3' :
+  ensure => link,
+  target  => '/tmp/testfile1',
+}
+
+dfaro {'dfaro' :
+  ensure => absent,
+}
+
+notify {"te lo notifico":}
+notify {"soy yo mismo bitch!!!!!":}
